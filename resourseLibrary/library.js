f90c7fa3cf162eb0e7410082f00a1dcb1b3d9074
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-03-08 00:08:23 +0530

    Add read less functionality

---
+++ resourseLibrary/library.js
@@ -0,0 +1,3 @@
+const blogId = '9190905892112183495';
+const apiKey = 'AIzaSyBSAHGIzn_IHOPwIFTtE0zbY9HSmwg1olY';
+const apiUrl = `https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}`;
